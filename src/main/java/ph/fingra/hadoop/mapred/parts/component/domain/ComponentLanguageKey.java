debab8a374c41b6506c3a7494a2cbf76f853bb00
Tae-woo Kim <kim@tgrape.com>
2014-10-02 17:36:10 +0900

    Add a MapReduce job to proceed "Component/ComponentLanguage".

---
+++ src/main/java/ph/fingra/hadoop/mapred/parts/component/domain/ComponentLanguageKey.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2014 tgrape Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ph.fingra.hadoop.mapred.parts.component.domain;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import ph.fingra.hadoop.mapred.common.BaseWritableComparable;
+
+public class ComponentLanguageKey extends BaseWritableComparable<ComponentLanguageKey> {
+    
+    public String appkey = "";
+    public String componentkey = "";
+    public String language = "";
+    public String token = "";
+    public String session = "";
+    
+    public void set(String appkey, String componentkey, String language, String token, String session) {
+        
+        this.appkey = appkey;
+        this.componentkey = componentkey;
+        this.language = language;
+        this.token = token;
+        this.session = session;
+    }
+    
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        
+        this.appkey = in.readUTF();
+        this.componentkey = in.readUTF();
+        this.language = in.readUTF();
+        this.token = in.readUTF();
+        this.session = in.readUTF();
+    }
+    
+    @Override
+    public void write(DataOutput out) throws IOException {
+        
+        out.writeUTF(this.appkey);
+        out.writeUTF(this.componentkey);
+        out.writeUTF(this.language);
+        out.writeUTF(this.token);
+        out.writeUTF(this.session);
+    }
+    
+    @Override
+    public int compareTo(ComponentLanguageKey o) {
+        
+        int ret = 0;
+        
+        ret = this.appkey.compareTo(o.appkey); if (ret != 0) return ret;
+        ret = this.componentkey.compareTo(o.componentkey); if (ret != 0) return ret;
+        ret = this.language.compareTo(o.language); if (ret != 0) return ret;
+        ret = this.token.compareTo(o.token); if (ret != 0) return ret;
+        ret = this.session.compareTo(o.session);
+        
+        return ret;
+    }
+    
+    @Override
+    public int hashCode() {
+        
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
